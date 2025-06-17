07fd592915c7466bafedbd3326afdc2cf5ef8667
Nairi-Khachatryan <nairi.khachatryan357@gmail.com>
2025-06-17 10:21:21 +0400

    add some styles

--- src/components/notFoundPage/index.tsx
+++ src/components/notFoundPage/index.tsx
@@ -1,12 +1,14 @@
 import notFoundImg from '../../images/404-error.jpg';
+import { useNavigate } from 'react-router-dom';
 import { Button } from 'antd';
 import './index.css';
 
 const NotFoundComponent = () => {
+  const navigate = useNavigate()
   return (
     <div className="image-wrapper">
       <img src={notFoundImg} alt="404 Error" />
-      <Button type="primary" className="button-home">
+      <Button onClick={() => navigate('/')} type="primary" className="button-home">
         Back to Home
       </Button>
     </div>
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,41 +1,49 @@
-import SecurityTab from './securityTab/SecurityTab';
+import { Avatar, Col, Row, Tabs, Typography } from 'antd';
 import ActivityTab from './activityTab/ActivityTab';
+import SecurityTab from './securityTab/SecurityTab';
+import { UserOutlined } from '@ant-design/icons';
 import ProfileTab from './profileTab/ProfileTab';
-import { Tabs, Row, Col } from 'antd';
 import React from 'react';
 import './Profile.css';
 
-export const Profile: React.FC = () => {
-  let theme = '';
+const { Title, Text } = Typography;
 
+export const Profile: React.FC = () => {
   const tabItems = [
     {
       key: '1',
       label: 'Profile',
       children: <ProfileTab />,
     },
-
     {
-      key: '4',
+      key: '2',
       label: 'Security',
       children: <SecurityTab />,
     },
     {
-      key: '5',
+      key: '3',
       label: 'Activity',
       children: <ActivityTab />,
     },
   ];
 
   return (
-    <div
-      className={`profile-page ${
-        theme === 'dark' ? 'profile-dark' : 'profile-light'
-      }`}
-    >
-      <Row className="container mx-auto px-4 py-8">
-        <Col span={24}>
-          <Tabs defaultActiveKey="1" className="custom-tabs" items={tabItems} />
+    <div className="profile-page" >
+      <Row className="profile-row" gutter={[32, 32]} align="middle">
+        <Col className='profile-tab-items' xs={24} md={6} style={{ textAlign: 'center' }}>
+          <Avatar size={120} icon={<UserOutlined />} />
+          <Title level={4} style={{ marginTop: 16 }}>
+            Nairi Khachatryan
+          </Title>
+          <div style={{ marginTop: 8 }}>
+            <Text>Email: nairi.khachatryan357@gmail.com</Text>
+            <br />
+            <Text>Location: Yerevan</Text>
+          </div>
+        </Col>
+
+        <Col className="profile-tabs-wrapper" xs={24} md={18}>
+          <Tabs defaultActiveKey="1" items={tabItems} style={{minWidth: 500}} />
         </Col>
       </Row>
     </div>
